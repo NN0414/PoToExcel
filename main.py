33d87f397ab66bf4026825dc4103944e4e4e5899
NN0414 <32450739+NN0414@users.noreply.github.com>
2024-05-25 22:39:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import polib
+import os
 from openpyxl import Workbook
 
 def export_to_excel(po_file_path, excel_file_path):
+    # Before conversion, remove existing file
+    if os.path.exists(excel_file_path):
+        os.remove(excel_file_path)
+    
     # Read the PO file
     po = polib.pofile(po_file_path)
 
@@ -10,16 +15,16 @@ def export_to_excel(po_file_path, excel_file_path):
     ws = wb.active
 
     # Write headers
-    ws.append(['msgid_plural_or_msgid', 'type', 'msgstr_index', 'msgstr'])
+    ws.append(['msgid', 'msgid_plural', 'msgstr_index', 'msgstr'])
 
     # Write data rows
     for entry in po:
         if entry.msgid_plural:
             for idx, msgstr in enumerate(entry.msgstr_plural.values()):
-                ws.append([entry.msgid_plural, 'msgid_plural', idx, msgstr])
+                ws.append([entry.msgid, entry.msgid_plural, idx, msgstr])
         else:
-            ws.append([entry.msgid, 'msgid', 0, entry.msgstr])
-
+            ws.append([entry.msgid, ' ', 0, entry.msgstr])
+    
     # Save the workbook
     wb.save(excel_file_path)
 
